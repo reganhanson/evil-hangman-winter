03359cd72de2e64bee6dafb928ce564dcde26ddf
reganhanson <reganhanson1994>
2021-02-08 09:57:40 -0700

    Basic functionality and error checking

--- src/hangman/EvilHangman.java
+++ src/hangman/EvilHangman.java
@@ -1,8 +1,35 @@
 package hangman;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class EvilHangman {
 
     public static void main(String[] args) {
+        String dictionaryFileName = args[0];
+        int wordLength = Integer.parseInt(args[1]);
+        int numGuesses = Integer.parseInt(args[2]);
+
+        Scanner scan = new Scanner(System.in);
+        try {
+            if (numGuesses <= 0) {
+                System.out.println("Invalid number of guesses");
+                throw new IOException();
+            }
+        } catch(IOException e)  {
+            e.printStackTrace();
+        }
+
+        File dictionaryFile = new File(dictionaryFileName);
+        EvilHangmanGame newGame = new EvilHangmanGame();
+        try {
+            newGame.startGame(dictionaryFile, wordLength);
+        } catch (IOException | EmptyDictionaryException e) {
+            e.printStackTrace();
+        }
+
+        // user input loop
 
     }
 
--- src/hangman/EvilHangmanGame.java
+++ src/hangman/EvilHangmanGame.java
@@ -2,13 +2,45 @@ package hangman;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class EvilHangmanGame implements IEvilHangmanGame{
+    // static variables
+    // variables
+    private int wordSize;
+    Set<String> dictionary;
+    SortedSet<Character> guessedLetters;
+
+    // constructors
+    public EvilHangmanGame() {
+        this.wordSize = 0;
+        this.dictionary = new TreeSet<>();
+        this.guessedLetters = new TreeSet<>();
+    }
+
+    // methods
     @Override
     public void startGame(File dictionary, int wordLength) throws IOException, EmptyDictionaryException {
+        Scanner scanFile = new Scanner(dictionary);
+        this.wordSize = wordLength;
 
+        if (this.wordSize < 2) {
+            throw new IOException();
+        }
+
+        // create dictionary with given text file
+        while (scanFile.hasNext()) {
+            String nextWord = scanFile.next();
+            if (nextWord.length() == this.wordSize) {
+                this.dictionary.add(nextWord);
+            }
+        }
+        if (this.dictionary.isEmpty()) {
+            throw new EmptyDictionaryException();
+        }
     }
 
     @Override
@@ -18,6 +50,6 @@ public class EvilHangmanGame implements IEvilHangmanGame{
 
     @Override
     public SortedSet<Character> getGuessedLetters() {
-        return null;
+        return guessedLetters;
     }
 }
