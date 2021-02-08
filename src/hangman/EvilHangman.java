c59966ca908f988cdd2fba2499295a44a74e355b
reganhanson <reganhanson1994>
2021-02-08 10:54:51 -0700

    Basic functionality and error checking

--- src/hangman/EvilHangman.java
+++ src/hangman/EvilHangman.java
@@ -11,7 +11,6 @@ public class EvilHangman {
         int wordLength = Integer.parseInt(args[1]);
         int numGuesses = Integer.parseInt(args[2]);
 
-        Scanner scan = new Scanner(System.in);
         try {
             if (numGuesses <= 0) {
                 System.out.println("Invalid number of guesses");
@@ -29,8 +28,21 @@ public class EvilHangman {
             e.printStackTrace();
         }
 
-        // user input loop
+        Scanner scan = new Scanner(System.in);
 
+        // user input loop
+        for (int i = 0; i < numGuesses; i++) {
+            System.out.printf("You have %d guesses left", numGuesses);
+            System.out.print("Used letters: ");
+            System.out.print("Word: ");
+            System.out.println("Enter guess: ");
+            char guess = scan.next().charAt(0);
+            try {
+                newGame.makeGuess(guess);
+            } catch(GuessAlreadyMadeException g) {
+                g.printStackTrace();
+            }
+        }
     }
 
 }
